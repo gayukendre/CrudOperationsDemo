01698b82df96dbcd084824b4c7309e685ef51373
HP <HP@GAYATRI>
2026-01-22 08:51:03 +0530

    Add project files.

---
+++ CrudOperationsDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrudOperationsDemo
+{
+    internal class Program
+    {
+        void Display()
+        {
+            Console.WriteLine("Welcome to Pune");
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
